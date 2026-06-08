2ff19916a568a6ae71cec69a1884b72fa34c7862
Macbook <macbook@192.168.1.17>
2026-06-08 23:17:38 +0800

    first commit

---
+++ hello.py
@@ -0,0 +1,120 @@
+# school= "university " 
+# #print(school * 20)
+# print(school.__mul__ (1000))
+# name="joe c"
+# print(name*100)
+# print(name.__mul__(1000))
+# is_raining=False
+# print(is_raining)
+# num=13.0
+# print(num.is_integer())
+# name = "Yeukai"
+# fruits ='bananas and apples'
+# print(name + " likes " +fruits)
+# print(f"My name is {name} and I like {fruits}")
+# x=10
+# score=20
+# print(score)
+# is_raining=True
+# if is_raining:
+#      print( "It is raining")
+# school= "university"
+# if school=="university":
+#     print("is school university")
+#     print("enrolled")
+#     print("next year")
+# age=35
+# if age >30:
+#         print("get married")
+# elif age<=33:
+#         print("you are on time")
+# elif age<=28 and age>20:
+#     print("okay")
+# elif age<16:
+#     print("very young")
+
+#loop
+# print(range(0,5))
+# for s in range(5):
+#       print
+
+# for _ in range(11):
+#       print("Malachi")
+# print(type(range))
+
+# for y in range(12):
+#       print("me")
+# y = 20
+# while y > 0:
+#       y = y - 2
+#       if y == 6:
+#            continue
+#       print(y)
+# for x in range(5):
+#       print('hello')
+# i=0
+# fruits=[]
+# print(type(fruits))
+# fruits= ['apple', 'banana', 'mango', 'guava']
+# print(callable(fruits))
+# print(callable(range))
+# print(callable(dir))
+# phone_book = {'joe':'033270520','Tina':'0412668340','Mali':'0327748821'}
+# print(phone_book['joe'])
+# player ={"first_one":'Joe' , 'age' : 25 , 'number': 8}
+# # print(dir(player))
+# print(player)
+# # player2 = player.copy
+# # print(player2)
+# player2 = dict.fromkeys
+# print(player2) 
+# for x in player:
+#     print(x)
+# student = {'first_name': 'paul ' , 'scores':[70, 90], 'age':30}
+# for keys, values in student:
+#     print(keys, values)
+# days=('Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday')
+# for x in days:
+#     print(x)
+# nums={1, 2 ,3,4}   
+# # nums.clear()
+# # print(nums)
+# del nums
+# print(nums)
+# help(input)
+# value = input()
+# print(value)
+# 'def self_intro(name='joe', surname= 'Mufudza'):
+#     # return f"MY NAME IS JOE     "
+
+#      print(self_intro())
+# # print(self_intro('Tina'))'
+
+import my_modules as mm
+
+# print(dir(my_modules))
+# answer = my_modules.multiply_two(200,5)
+# print(answer)
+# print(mm.first_name)
+# print(mm .favourite_car)
+# print(mm.favourite_food)
+
+# import random
+import numpy
+print(numpy.__version__)
+# print("This is a guessing game")
+# computer_pick = random.randint(0, 10)
+# playing=True
+# while playing:
+#      print("Guess a number between 0 and 10 inclusive")
+#      guess = input()
+#      print(f'Your guess is: {guess}')
+
+# if guess == computer_pick:
+#     print('you won the game')
+#     print('you loose')
+#     print('computer_pick')
+#     playing= False
+# else:
+#     print('try another number')
+   
